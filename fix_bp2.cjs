e98fa4af9662afc0927a1b8137ee870254f6fede
sipix41 <sipix41@gmail.com>
2026-06-06 03:09:28 -0400

    chore: remove unused utility scripts
    Delete obsolete automation and maintenance scripts used for site generation.

--- fix_bp2.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('pages/BP.tsx', 'utf8');
-content = content.replace(
-  /<\/a>\s*<\/div>\s*<\/div>\s*<\/section>/,
-  '</a>\n          </div>\n          </motion.div>\n        </div>\n      </section>'
-);
-fs.writeFileSync('pages/BP.tsx', content, 'utf8');
